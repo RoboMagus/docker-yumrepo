7cbe6c4c8b61e9186d6ff42098bae5405dbaeb2a
RoboMagus <its@bogus.com>
2021-10-01 11:00:47 +0200

    Github build hook to push Docker image to docker hub Added default arguments for yumrepo script (port and protocol) Createrepo now works for symlinks

--- extras/yumrepo.sh
+++ extras/yumrepo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+# Set defaults for required ENV variables:
+: ${REPO_PROTO:=http}
+: ${REPO_PORT:=80}
+
 CONTAINER_IP=$(ip addr show eth0 | awk '$1 == "inet" {gsub(/\/.*$/, "", $2); print $2}')
 NGINX_PID=undef
 
@@ -37,8 +41,7 @@ function createRepos(){
     # - as a workaround delete these files before calling createrepo_c - they will be recreated afterwards anyway
     [ -n "${REPO_GPG_KEY_NAME}" ] && find ${REPO_PATH} -name 'repomd.xml.*' -type f -exec rm {} \;
 
-    find ${REPO_PATH} -type d -maxdepth ${REPO_DEPTH} -mindepth ${REPO_DEPTH} -exec createrepo_c {} \;
-
+    find -L ${REPO_PATH} -type d -maxdepth ${REPO_DEPTH} -mindepth ${REPO_DEPTH} -exec echo "Creating repo for {}" \; -exec createrepo_c --update {} \;
     if [ -n "${REPO_GPG_KEY_NAME}" ]; then
         find ${REPO_PATH} -type f -name 'repomd.xml' -print |
         while read repomd; do
@@ -66,11 +69,14 @@ function serveRepos(){
 
 importGpgKey
 printRepoConf
-createRepos
 serveRepos
+createRepos
 
 inotifywait -m -r -e create -e delete -e delete_self --excludei '(repodata|.*xml)' ${REPO_PATH}/ |
 while read path action file; do
     echo -e "> Repository content was changed..."
+    echo "path: $path"
+    echo "action: $action"
+    echo "file: $file"
     createRepos
 done
